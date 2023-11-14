0456a3983244d843f64cd46afbd5126810176536
Angel-Men25 <angel.valdez.25@hotmail.com>
2023-11-13 19:29:24 -0600

    Añade diseño responsivo

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,13 @@ const closeMenuBtn = document.querySelector('.close-menu');
 const menuLinks = document.querySelectorAll('.hero__link');
 
 openMenuBtn.addEventListener('click', () => {
-  menu.classList.add('show')
+  menu.classList.add('show');
+  document.body.classList.add('no-scroll');
 })
 
 closeMenuBtn.addEventListener('click', () => {
-  menu.classList.remove('show')
+  menu.classList.remove('show');
+  document.body.classList.remove('no-scroll');
 })
 
 menuLinks.forEach(link => {
